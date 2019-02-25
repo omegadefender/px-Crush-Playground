13732a93d0f6cd7976ba8a81951aa07ddd10c64c
James H <hastings.james.m@gmail.com>
2019-02-25 16:56:52 +1100

    Restructured dirs and started on Navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../css_files/navbar.css'
+
+class Navbar extends React.Component {
+    render() {
+
+        return (
+           <header>
+               <nav class="nav-content"></nav>
+                    <div class="logo"></div>
+           </header>
+            
+        )}
+}
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './css_files/index.css'
+import * as serviceWorker from './serviceWorker'
+import Navbar from './components/navbar'
 
-
+ReactDOM.render(
+    <Navbar />,
+    document.getElementById('navigation')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
